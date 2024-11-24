8a1f21dbb27af760ee0a419ddf10517078afa30a
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-24 01:42:17 -0500

    Update pag_principal.py

--- SEGUNDO_PROYECTO/pag_principal.py
+++ SEGUNDO_PROYECTO/pag_principal.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import streamlit as st
+from colores import estilos_css  # Importar los estilos desde colores.py
 
-
-
+# Función para la página principal
 def pagina_principal():
+    # Inyectar los estilos CSS personalizados
+    st.markdown(estilos_css, unsafe_allow_html=True)
+
     st.title("Recihome")
     st.write("Una app centrada en la visualización, recolección y reciclaje de residuos en el hogar, "
              "permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.")
