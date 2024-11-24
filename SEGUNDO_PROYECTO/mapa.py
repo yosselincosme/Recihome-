3bd0259d54e98a7077f036f3e02b3b99c490630e
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-24 02:42:48 -0500

    Update mapa.py

--- SEGUNDO_PROYECTO/mapa.py
+++ SEGUNDO_PROYECTO/mapa.py
@@ -1,37 +1,46 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import geopandas as gpd
 import plotly.graph_objects as go
+import geopandas as gpd
 import json
+import requests
 
-# Función para mostrar el mapa interactivo
+# Función para cargar y procesar el mapa con los datos de residuos
 def mostrar_mapa(archivo_cargado):
-    # Cargar el archivo GeoJSON de Perú
-    geojson_url = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/peru-departments.geojson"
-    geojson_data = gpd.read_file(geojson_url)
+    # Cargar el archivo GeoJSON de las regiones de Perú
+    geojson_url = "https://raw.githubusercontent.com/Sawamurarebatta/Recihome-/main/SEGUNDO_PROYECTO/peru_regions.geojson"
+    geojson_data = requests.get(geojson_url).json()  # Usamos requests para cargar el GeoJSON
 
     # Filtrar los datos del CSV para obtener los residuos por departamento
     residuos_por_departamento = archivo_cargado.groupby('DEPARTAMENTO').sum().reset_index()
 
-    # Asegurarse de que las columnas para los residuos están presentes
+    # Asegurarse de que las columnas de residuos están presentes
     residuos_columnas = ['QRESIDUOS_DOM', 'QRESIDUOS_COM', 'QRESIDUOS_INDUSTRIAL']  # Ajusta según tus columnas
 
     # Crear un DataFrame para los residuos por departamento
     residuos_totales = residuos_por_departamento[["DEPARTAMENTO"] + residuos_columnas]
     residuos_totales = residuos_totales.set_index("DEPARTAMENTO")
 
-    # Unir los datos del CSV con los datos geojson por el nombre del departamento
-    geojson_data['properties']['name'] = geojson_data['properties'].apply(lambda x: x['name'])
-    geojson_data = geojson_data.merge(residuos_totales, left_on='properties.name', right_index=True)
+    # Procesar el GeoJSON y combinar con los datos de residuos
+    for feature in geojson_data['features']:
+        departamento_name = feature['properties']['name']  # Nombre del departamento en el GeoJSON
+        if departamento_name in residuos_totales.index:
+            feature['properties']['residuos_dom'] = residuos_totales.loc[departamento_name, 'QRESIDUOS_DOM']
+            feature['properties']['residuos_com'] = residuos_totales.loc[departamento_name, 'QRESIDUOS_COM']
+            feature['properties']['residuos_industrial'] = residuos_totales.loc[departamento_name, 'QRESIDUOS_INDUSTRIAL']
+        else:
+            feature['properties']['residuos_dom'] = 0
+            feature['properties']['residuos_com'] = 0
+            feature['properties']['residuos_industrial'] = 0
 
-    # Crear un gráfico de mapa interactivo usando Plotly
+    # Crear el gráfico de mapa interactivo con Plotly
     fig = go.Figure(go.Choroplethmapbox(
-        geojson=geojson_data.geometry.__geo_interface__,
-        locations=geojson_data.index,
-        z=geojson_data['QRESIDUOS_DOM'],  # Puedes cambiar a la columna deseada
+        geojson=geojson_data,
+        locations=[feature['properties']['name'] for feature in geojson_data['features']],
+        z=[feature['properties']['residuos_dom'] for feature in geojson_data['features']],
         colorscale="Viridis",
-        colorbar_title="Cantidad de Residuos Domésticos",
+        colorbar_title="Residuos Domésticos",
     ))
 
     fig.update_layout(
@@ -43,4 +52,5 @@ def mostrar_mapa(archivo_cargado):
     )
 
     st.plotly_chart(fig)
+
 
