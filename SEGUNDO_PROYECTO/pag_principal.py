8d1c538607559f0550c3482104e2b87094d72dc8
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-23 23:50:32 -0500

    Update pag_principal.py

--- SEGUNDO_PROYECTO/pag_principal.py
+++ SEGUNDO_PROYECTO/pag_principal.py
@@ -7,12 +7,4 @@ def pagina_principal():
              "permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.")
     st.write("Usa el menú de la izquierda para navegar entre páginas.")
 
-    # Cargar el archivo CSV con el delimitador correcto
-    try:
-        df = pd.read_csv(r"C:\Users\Yosselin\Desktop\SEGUNDO_PROYECTO\residuos.csv", sep=';', encoding='latin1')
-        st.write("### Datos de residuos:")
-        st.dataframe(df)
-    except FileNotFoundError:
-        st.error("No se encontró el archivo `residuos.csv`. Asegúrate de que la ruta sea correcta.")
-    except UnicodeDecodeError:
-        st.error("No se pudo decodificar el archivo. Intenta usar otra codificación como 'latin1' o 'ISO-8859-1'.")
+   
