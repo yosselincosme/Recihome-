d068a6b88a215de83f75e4899caa05d51ea20b6e
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-24 02:02:43 -0500

    Update pag_principal.py

--- SEGUNDO_PROYECTO/pag_principal.py
+++ SEGUNDO_PROYECTO/pag_principal.py
@@ -7,9 +7,18 @@ def pagina_principal():
     # Inyectar los estilos CSS personalizados
     st.markdown(inyectar_estilos(), unsafe_allow_html=True)  # Asegúrate de ejecutar la función
 
+    # Título de la aplicación
     st.title("Recihome")
+    
+    # Descripción de la aplicación
     st.write("Una app centrada en la visualización, recolección y reciclaje de residuos en el hogar, "
              "permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.")
+    
     st.write("Usa el menú de la izquierda para navegar entre páginas.")
 
-
+    # Imagen en la esquina inferior derecha
+    st.markdown("""
+        <div style="position: fixed; bottom: 10px; right: 10px; z-index: 999;">
+            <img src="https://raw.githubusercontent.com/Sawamurarebatta/Recihome-/main/SEGUNDO_PROYECTO/planeta.png" width="150">
+        </div>
+    """, unsafe_allow_html=True)
