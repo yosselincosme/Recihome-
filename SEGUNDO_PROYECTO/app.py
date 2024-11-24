1d3df6fdcdff29e9169c628e4f1535758888c824
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-24 00:09:14 -0500

    Update app.py

--- SEGUNDO_PROYECTO/app.py
+++ SEGUNDO_PROYECTO/app.py
@@ -11,6 +11,7 @@ archivo_cargado = pd.read_csv(url, sep=';', encoding='latin1')
 
 # Crear el menú horizontal
 selected = option_menu(
+    menu_title=None,
     options=["Página principal", "Distribución general", "Mapa", "Resumen", "Análisis por Departamento"],  # Nueva opción
     icons=["house", "bar-chart", "map", "clipboard", "filter"],  # Ícono para la nueva opción
     menu_icon="cast",  # Ícono general del menú
