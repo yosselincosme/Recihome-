25509f913d172cadae75a16681da9e7f070025bd
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-23 23:39:28 -0500

    Update app.py

--- SEGUNDO_PROYECTO/app.py
+++ SEGUNDO_PROYECTO/app.py
@@ -6,7 +6,7 @@ from pag_principal import pagina_principal
 from distribucion_general import distribucion_general
 
 # Cargar el archivo CSV
-archivo_cargado = pd.read_csv("./residuos.csv", sep=';', encoding='latin1')
+archivo_cargado = pd.read_csv("./residuos.csv")
 
 # Crear el menú horizontal
 selected = option_menu(
