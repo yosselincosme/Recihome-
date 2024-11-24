4c71b6b84a7cea65eb985284113053ada2e207a6
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-24 01:37:26 -0500

    Update pag_principal.py

--- SEGUNDO_PROYECTO/pag_principal.py
+++ SEGUNDO_PROYECTO/pag_principal.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
-from colores import colores  # Importa el diccionario de colores
+from colores import colores
+
 
 def pagina_principal():
     st.title("Recihome")
@@ -8,40 +9,4 @@ def pagina_principal():
              "permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.")
     st.write("Usa el menú de la izquierda para navegar entre páginas.")
 
-# Función para inyectar los estilos CSS personalizados
-def inyectar_estilos():
-    return f"""
-    <style>
-    /* Fondo principal */
-    .stApp {{
-        background-color: {colores['fondo_principal']};
-    }}
-    
-    /* Fondo del sidebar */
-    [data-testid="stSidebar"] {{
-        background-color: {colores['fondo_sidebar']};
-    }}
-    
-    /* Encabezados */
-    h1, h2, h3, h4 {{
-        color: {colores['encabezado']};
-    }}
-    
-    /* Botones y enlaces */
-    .css-1q8dd3e, .css-1aumxhk, .st-cv {{
-        background-color: {colores['boton']} !important;
-        color: {colores['fondo_principal']} !important;
-        border: 1px solid {colores['encabezado']} !important;
-    }}
-    
-    /* Texto general */
-    .css-16huue1 {{
-        color: {colores['texto_general']} !important;
-    }}
-    </style>
-    """
-    
-# Inyectar los estilos en la app
-st.markdown(inyectar_estilos(), unsafe_allow_html=True)
 
-   
