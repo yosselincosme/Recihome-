a560f54aadf00919ef5aad94fe5cb33b4b547d81
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-24 00:46:18 -0500

    Update colores.py

--- SEGUNDO_PROYECTO/colores.py
+++ SEGUNDO_PROYECTO/colores.py
@@ -1,15 +1,12 @@
-# colores.py
-
 # Paleta de colores para la aplicación
 colores = {
-    'fondo_principal': '#E9EFEC',
-    'fondo_sidebar': '#C4DAD2',
-    'encabezado': '#16423C',
-    'boton': '#6A9C89',
-    'texto_general': '#16423C',
-    'grafico': ['#16423C', '#6A9C89', '#C4DAD2', '#E9EFEC']
+    'fondo_principal': '#16423C',  # Verde oscuro
+    'fondo_sidebar': '#C4DAD2',    # Verde claro
+    'encabezado': '#FFFFFF',       # Blanco
+    'boton': '#6A9C89',            # Verde medio
+    'texto_general': '#FFFFFF',    # Blanco
+    'grafico': ['#16423C', '#6A9C89', '#C4DAD2', '#E9EFEC']  # Colores para gráficos
 }
-
 # Función para inyectar los estilos CSS personalizados
 def inyectar_estilos():
     return f"""
@@ -42,3 +39,4 @@ def inyectar_estilos():
     }}
     </style>
     """
+
