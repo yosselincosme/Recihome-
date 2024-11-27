a7b3c6af5ef68053649bfad2958f4cf51d461b62
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-27 16:24:45 -0500

    Update app.py

--- SEGUNDO_PROYECTO/app.py
+++ SEGUNDO_PROYECTO/app.py
@@ -6,9 +6,6 @@ from mapa import mostrar_mapa  # Importar la función mostrar_mapa desde mapa.py
 from distribucion_general import distribucion_general  # Distribución general (si es necesario agregarla)
 from pag_principal import pagina_principal  # Página principal (si es necesario)
 
-# Inyectar estilos CSS globalmente
-st.markdown(inyectar_estilos(), unsafe_allow_html=True)
-
 # Cargar archivo CSV
 url = "https://raw.githubusercontent.com/Sawamurarebatta/Recihome-/main/SEGUNDO_PROYECTO/residuos.csv"
 archivo_cargado = pd.read_csv(url, sep=';', encoding='latin1')
@@ -21,7 +18,11 @@ selected = option_menu(
     menu_icon="cast",
     default_index=0,
     orientation="horizontal",
-    styles={"container": {"max-width": "300%", "padding": "10px 0"}},  # Estilo del menú
+    styles={
+            "container": {"padding": "0!important", "background-color": "#fafafa"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "blue"},  # Estilo del menú
 )
 
 # Mostrar contenido según la opción seleccionada
