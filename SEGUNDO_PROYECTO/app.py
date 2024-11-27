6ededac7645439ab1d3beb62ab6dbff0754e049a
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-27 17:03:09 -0500

    Update app.py

--- SEGUNDO_PROYECTO/app.py
+++ SEGUNDO_PROYECTO/app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -7,33 +6,37 @@ from pag_principal import pagina_principal
 from distribucion_general import distribucion_general, grafico_lineal_por_periodo
 from filtros_avanzados import filtros_avanzados
 from mapa import dashboard_residuos
-
+from colores import obtener_css  # Importar la función para obtener el CSS
 
 # Configurar la página
-st.set_page_config(layout="wide")  # Aquí se configura el layout de la página para usar el espacio completo
+st.set_page_config(layout="wide")
 
+# Agregar CSS para personalizar la apariencia
+st.markdown(obtener_css(), unsafe_allow_html=True)
 
+# Cargar datos
 url = "https://raw.githubusercontent.com/Sawamurarebatta/Recihome-/main/SEGUNDO_PROYECTO/residuos.csv"
 archivo_cargado = pd.read_csv(url, sep=';', encoding='latin1')
+
 # Crear el menú horizontal
 selected = option_menu(
-    menu_title="Menú Principal",  # Título del menú
-    options=["Página principal", "Distribución general", "Mapa", "Resumen", "Filtros Avanzados"],  # Nueva opción
-    icons=["house", "bar-chart", "map", "clipboard", "filter"],  # Ícono para la nueva opción
-    menu_icon="cast",  # Ícono general del menú
-    default_index=0,  # Opción predeterminada al cargar
-    orientation="horizontal",  # Menú horizontal
+    menu_title="Menú Principal",
+    options=["Página principal", "Distribución general", "Mapa", "Resumen", "Filtros Avanzados"],
+    icons=["house", "bar-chart", "map", "clipboard", "filter"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
     styles={
-            "container": {"padding": "0!important", "background-color": "#fafafa"},
-            "icon": {"color": "orange", "font-size": "25px"}, 
-            "nav-link": {"font-size": "10px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "blue"},
-        }
+        "container": {"padding": "0!important", "background-color": colores['fondo_sidebar']},
+        "icon": {"color": colores['boton'], "font-size": "25px"},
+        "nav-link": {"font-size": "10px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "blue"},
+    }
 )
 
 # Mostrar contenido según la opción seleccionada
 if selected == "Página principal":
-    pagina_principal()  # Llama a la función desde el archivo pag_principal.py
+    pagina_principal()
 
 elif selected == "Distribución general":
     st.title("Distribución general")
@@ -50,10 +53,8 @@ elif selected == "Resumen":
     geojson_url = 'https://github.com/Sawamurarebatta/Recihome-/blob/main/SEGUNDO_PROYECTO/peru_regions.geojson'
     depurar_geojson(geojson_url)
 
-elif selected == "Filtros Avanzados":  # Nueva sección
+elif selected == "Filtros Avanzados":
     filtros_avanzados(archivo_cargado)
-
-
 
 
 
