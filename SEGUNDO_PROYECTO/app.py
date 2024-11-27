011b9a75851c347987abc8d1db03375e19dca71e
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-27 16:59:26 -0500

    Update app.py

--- SEGUNDO_PROYECTO/app.py
+++ SEGUNDO_PROYECTO/app.py
@@ -26,7 +26,7 @@ selected = option_menu(
     styles={
             "container": {"padding": "0!important", "background-color": "#fafafa"},
             "icon": {"color": "orange", "font-size": "25px"}, 
-            "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link": {"font-size": "10px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
             "nav-link-selected": {"background-color": "blue"},
         }
 )
