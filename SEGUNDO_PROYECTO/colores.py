2ee6cab58e1149e8d9c4d4f7a69b45928aae86f2
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-29 23:20:49 -0500

    Update colores.py

--- SEGUNDO_PROYECTO/colores.py
+++ SEGUNDO_PROYECTO/colores.py
@@ -1,15 +1,15 @@
 colores = {
-    'fondo_principal': 'rgb(52, 58, 64)',  # Fondo de la pantalla principal
-    'fondo_sidebar': 'rgb(34, 34, 59)',  # Fondo del sidebar (menú)
-    'fondo_menu': 'rgb(52, 58, 64)',  # Fondo específico para el menú
-    'encabezado': 'rgb(52, 58, 64)',  # Blanco
-    'boton': 'rgb(154, 140, 152)',  # Verde medio
-    'texto_general': 'rgb(255, 255, 255)',  # Blanco
+    'fondo_principal': '#343A40',  
+    'fondo_sidebar': '#22223B',  #
+    'fondo_menu': '#343A40',  # 
+    'encabezado': '#343A40',  # 
+    'boton': '#9A8C98',  #
+    'texto_general': '#FFFFFF', 
     'grafico': [
-        'rgb(22, 66, 60)',
-        'rgb(106, 156, 137)',
-        'rgb(196, 218, 210)',
-        'rgb(233, 239, 236)',
+        '#16363C',  # 
+        '#6A9C89',  # 
+        '#C4DAD2',  # 
+        '#E9EFEC',  # 
     ]  # Colores para gráficos
 }
 
