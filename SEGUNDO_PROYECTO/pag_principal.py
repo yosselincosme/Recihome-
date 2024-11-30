e29737a13681f36aec97e472b58b576194632d76
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-30 00:18:07 -0500

    Update pag_principal.py

--- SEGUNDO_PROYECTO/pag_principal.py
+++ SEGUNDO_PROYECTO/pag_principal.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-
 # Función para la página principal
 def pagina_principal():
     # Inyectar los estilos CSS personalizados
+    gif_url = "https://github.com/Sawamurarebatta/Recihome-/blob/main/SEGUNDO_PROYECTO/Uonl.gif?raw=true"  # URL del GIF
 
     # Título de la aplicación
     st.title("Recihome")
@@ -14,6 +14,19 @@ def pagina_principal():
              "permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.")
     
     st.write("Usa el menú de la izquierda para navegar entre páginas.")
+
+    # Incluir el GIF como fondo
+    st.markdown(f"""
+        <style>
+            .stApp {{
+                background-image: url("{gif_url}");
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;
+                height: 100%;
+            }}
+        </style>
+    """, unsafe_allow_html=True)
 
     # Imagen en la esquina inferior derecha
     st.markdown("""
