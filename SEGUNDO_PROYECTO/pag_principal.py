23382d8b7943cfe3d0aa29c600862f0a48d7527c
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-30 00:20:25 -0500

    Update pag_principal.py

--- SEGUNDO_PROYECTO/pag_principal.py
+++ SEGUNDO_PROYECTO/pag_principal.py
@@ -4,28 +4,24 @@ import streamlit as st
 # Función para la página principal
 def pagina_principal():
     # Inyectar los estilos CSS personalizados
-    gif_url = "https://github.com/Sawamurarebatta/Recihome-/blob/main/SEGUNDO_PROYECTO/Uonl.gif?raw=true"  # URL del GIF
 
-    # Título de la aplicación
-    st.title("Recihome")
+    # Título de la aplicación con texto grande
+    st.markdown("""
+        <h1 style="font-size: 50px; text-align: center;">Recihome</h1>
+    """, unsafe_allow_html=True)
     
-    # Descripción de la aplicación
-    st.write("Una app centrada en la visualización, recolección y reciclaje de residuos en el hogar, "
-             "permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.")
+    # Descripción de la aplicación con texto más grande
+    st.markdown("""
+        <p style="font-size: 20px; text-align: justify;">
+            Una app centrada en la visualización, recolección y reciclaje de residuos en el hogar, 
+            permitiendo monitorear en tiempo real los residuos generados por los hogares en todo el Perú mediante gráficos y promedios de cada vivienda.
+        </p>
+    """, unsafe_allow_html=True)
     
-    st.write("Usa el menú de la izquierda para navegar entre páginas.")
-
-    # Incluir el GIF como fondo
-    st.markdown(f"""
-        <style>
-            .stApp {{
-                background-image: url("{gif_url}");
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center;
-                height: 100%;
-            }}
-        </style>
+    st.markdown("""
+        <p style="font-size: 18px; text-align: center;">
+            Usa el menú de la izquierda para navegar entre páginas.
+        </p>
     """, unsafe_allow_html=True)
 
     # Imagen en la esquina inferior derecha
