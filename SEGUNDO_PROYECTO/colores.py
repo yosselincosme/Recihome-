ca71726a1a84fa8e058b9b77c973944d779227d7
Sawamurarebatta <150810688+Sawamurarebatta@users.noreply.github.com>
2024-11-29 22:13:47 -0500

    Update colores.py

--- SEGUNDO_PROYECTO/colores.py
+++ SEGUNDO_PROYECTO/colores.py
@@ -1,15 +1,16 @@
-# colores.py
-
-# colors.py
-
 colores = {
-    'fondo_principal': '#16423C',  # Fondo de la pantalla principal
-    'fondo_sidebar': '#C4DAD2',    # Fondo del sidebar (menú)
-    'fondo_menu': '#FFFFFF',       # Fondo específico para el menú
-    'encabezado': '#FFFFFF',       # Blanco
-    'boton': '#6A9C89',            # Verde medio
-    'texto_general': '#FFFFFF',    # Blanco
-    'grafico': ['#16423C', '#6A9C89', '#C4DAD2', '#E9EFEC']  # Colores para gráficos
+    'fondo_principal': 'rgb(22, 66, 60)',  # Fondo de la pantalla principal
+    'fondo_sidebar': 'rgb(196, 218, 210)',  # Fondo del sidebar (menú)
+    'fondo_menu': 'rgb(255, 255, 255)',  # Fondo específico para el menú
+    'encabezado': 'rgb(255, 255, 255)',  # Blanco
+    'boton': 'rgb(106, 156, 137)',  # Verde medio
+    'texto_general': 'rgb(255, 255, 255)',  # Blanco
+    'grafico': [
+        'rgb(22, 66, 60)',
+        'rgb(106, 156, 137)',
+        'rgb(196, 218, 210)',
+        'rgb(233, 239, 236)',
+    ]  # Colores para gráficos
 }
 
 def obtener_css():
@@ -47,3 +48,4 @@ def obtener_css():
         }}
     </style>
     """
+
